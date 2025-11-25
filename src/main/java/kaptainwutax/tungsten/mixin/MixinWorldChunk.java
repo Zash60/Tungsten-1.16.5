59f67db2c268c6fc8aa90197c50f4295de2afa1c
Zash60 <xath06@gmail.com>
2025-11-25 17:10:05 -0300

    Update MixinWorldChunk.java

--- src/main/java/kaptainwutax/tungsten/mixin/MixinWorldChunk.java
+++ src/main/java/kaptainwutax/tungsten/mixin/MixinWorldChunk.java
@@ -7,9 +7,9 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.BiomeArray;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkSection;
 import net.minecraft.world.chunk.UpgradeData;
@@ -26,8 +26,7 @@ import java.util.function.Consumer;
 @Mixin(WorldChunk.class)
 public abstract class MixinWorldChunk extends Chunk {
 
-    // 1.16.5 constructor signature
-	public MixinWorldChunk(World world, ChunkPos pos, Biome[] biomes, UpgradeData upgradeData, @Nullable long[] inhabitedTime, @Nullable ChunkSection[] sections, @Nullable Consumer<WorldChunk> consumer) {
+	public MixinWorldChunk(World world, ChunkPos pos, BiomeArray biomes, UpgradeData upgradeData, @Nullable long[] inhabitedTime, @Nullable ChunkSection[] sections, @Nullable Consumer<WorldChunk> consumer) {
 		super(pos, biomes, upgradeData, world, inhabitedTime, sections, consumer);
 	}
 
@@ -36,10 +35,10 @@ public abstract class MixinWorldChunk extends Chunk {
 	@Shadow public abstract FluidState getFluidState(BlockPos pos);
 
 	@Inject(method = "loadFromPacket", at = @At("RETURN"))
-	private void loadFromPacket(@Nullable Biome[] biomes, PacketByteBuf buf, CompoundTag nbt, int verticalStripBitmask, CallbackInfo ci) {
+	private void loadFromPacket(@Nullable BiomeArray biomes, PacketByteBuf buf, CompoundTag nbt, int verticalStripBitmask, CallbackInfo ci) {
 		if(TungstenMod.WORLD == null || this.getWorld() != TungstenMod.WORLD.parent) {
 			return;
 		}
-        // Logic remains commented out as per source
 	}
+
 }
