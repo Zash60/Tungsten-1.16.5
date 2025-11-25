95ef780cb2e64b0e3299dd1e141a7d1b1cb4c8e7
Zash60 <xath06@gmail.com>
2025-11-25 17:50:38 -0300

    Update MixinWorldChunk.java

--- src/main/java/kaptainwutax/tungsten/mixin/MixinWorldChunk.java
+++ src/main/java/kaptainwutax/tungsten/mixin/MixinWorldChunk.java
@@ -3,7 +3,7 @@ package kaptainwutax.tungsten.mixin;
 import kaptainwutax.tungsten.TungstenMod;
 import net.minecraft.block.BlockState;
 import net.minecraft.fluid.FluidState;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.CompoundTag; // Na 1.16.5 yarn é CompoundTag
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -19,18 +19,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WorldChunk.class)
 public abstract class MixinWorldChunk {
 
-    // Shadows sao necessarios para acessar metodos da classe alvo
 	@Shadow public abstract World getWorld();
 	@Shadow public abstract BlockState getBlockState(BlockPos pos);
 	@Shadow public abstract FluidState getFluidState(BlockPos pos);
 
-    // Assinatura corrigida para 1.16.5
 	@Inject(method = "loadFromPacket", at = @At("RETURN"))
 	private void loadFromPacket(@Nullable BiomeArray biomes, PacketByteBuf buf, CompoundTag nbt, int verticalStripBitmask, CallbackInfo ci) {
 		if(TungstenMod.WORLD == null || this.getWorld() != TungstenMod.WORLD.parent) {
 			return;
 		}
-        // Logica comentada
 	}
 
 }
